d25f8ff1ef9153d4656b811e06033c692458f6c3
Gautam Kumar <gautam@Gautams-MacBook-Pro.local>
2017-03-11 19:37:56 +0530

    Initial commit

---
+++ playground/mongodd-find.js
@@ -0,0 +1,26 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Users').find().count().then((count) => {
+    //     console.log(`Todos Count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos');
+    // });
+
+    db.collection('Users').find({name : 'Gautam'})
+    .toArray()
+    .then((docs) => {
+      console.log(JSON.stringify(docs, undefined, 2));
+    }, (error) => {
+      console.log('Unable to fetch todos');
+    });
+
+    db.close();
+
+});
